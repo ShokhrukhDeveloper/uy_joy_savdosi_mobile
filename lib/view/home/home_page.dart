38b420ba870c32fa4b3166988dca9d579863c164
Shoxrux <shoxruxyarashovuz@gmail.com>
2024-01-14 12:29:23 +0500

    search filed ready

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -9,24 +9,72 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isSearch=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: const FlutterLogo(),
-        title: Text("Navoiy uy joy"),
+        title:!isSearch?const Text("Navoiy uy joy"):
+        TextField(
+          onSubmitted: (text){
+            print(text);
+            print("---------------------------------------------------------------------------");
+          },
+          decoration: InputDecoration(
+              contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15.0),
+            border: OutlineInputBorder(
+              gapPadding: 0,
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(color: Colors.white38)
+            ),
+            focusedBorder: OutlineInputBorder(
+                gapPadding: 0,
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide(color: Colors.black26)
+            ),
+          ),
+        ),
+        // ,
         centerTitle: true,
         actions: [
-          Container(
-            margin: EdgeInsets.only(right: 10),
-            width: 40,
-            height: 40,
-            decoration: BoxDecoration(
-              border: Border.all(color: Color(0xffE2E8F0),width: 2),
-              borderRadius: BorderRadius.circular(8)
+          if(!isSearch)InkWell(
+            onTap: (){
+              isSearch=true;
+              setState(() {
+
+              });
+            },
+            child: Container(
+              margin: EdgeInsets.only(right: 10),
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xffE2E8F0),width: 2),
+                borderRadius: BorderRadius.circular(8)
+              ),
+              child: Icon(Icons.search_rounded),
             ),
-            child: Icon(Icons.search_rounded),
           ),
+          if(isSearch)InkWell(
+            onTap: (){
+              isSearch=false;
+              setState(() {
+
+              });
+            },
+            child: Container(
+              margin: EdgeInsets.only(right: 10),
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xffE2E8F0),width: 2),
+                  borderRadius: BorderRadius.circular(8)
+              ),
+              child: Icon(Icons.clear),
+            ),
+          ),
+
           Container(
             margin: EdgeInsets.only(right: 10),
             width: 40,
@@ -54,7 +102,7 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.white,
                 child: InkWell(
                     onTap: (){
-                      Navigator.push(context as BuildContext, MaterialPageRoute(builder: (context)=>ProductPage()));
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const ProductPage()));
                     },
                     child: HomeItemWidget()),
               );
